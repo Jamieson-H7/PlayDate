72d34fb76784125ddd1df83609ceef8afcf8c967
Jamieson-H7 <114550501+Jamieson-H7@users.noreply.github.com>
2023-10-10 12:25:38 +0000

    css and js

---
+++ main.js
@@ -0,0 +1,42 @@
+window.onload = init;
+
+var dateButton;
+var firstDateInput;
+var parentDiv;
+var dateContainer;
+
+function init(){
+    initInputs = document.getElementsByClassName("dateInputs");
+    for(item of initInputs){
+        item.min = new Date().toISOString().split("T")[0];
+    }
+    dateButton = document.getElementById("addDateButton");
+    firstDateInput = document.getElementById("firstDateInput");
+    dateContainer = document.getElementById("dateContainer");
+    parentDiv = firstDateInput.parentNode;
+}
+
+// dateButton.addEventListener("click", dateButtonFunct);
+
+function dateButtonFunct() {
+    // console.log("bruh")
+    // newRow = document.createElement("tr");
+    // newCol = document.createElement("td");
+    // newDateInput = document.createElement("input");
+    // newDateInput.type = "date";
+    // newDateInput.min = new Date().toISOString().split("T")[0];
+    // newDateInput.classList.add("dateInputs");
+    // // type="date" id="start"
+    
+    // newCol.appendChild(newDateInput);
+    // newRow.appendChild(newCol);
+    // parentDiv.insertBefore(newRow, firstDateInput);
+
+    newDateInput = document.createElement("input");
+    newDateInput.type = "date";
+    newDateInput.min = new Date().toISOString().split("T")[0];
+    newDateInput.classList.add("dateInputs");
+    newDateInput.classList.add("fadeIn");
+    newDateInput.addEventListener('animationend', function(e) {$(newDateInput).removeClass("fadeIn");});
+    dateContainer.appendChild(newDateInput);
+}
