5e44f94fdcd420539568652a28091e35b372a4f4
Jamieson-H7 <114550501+Jamieson-H7@users.noreply.github.com>
2023-10-24 13:26:00 +0000

    added a dot (window location)

--- appStuff.js
+++ appStuff.js
@@ -155,7 +155,7 @@ function initial() {
       
       addUserData(temp2, ref(database, 'events/' + nameInput.value));
       addUserData(temp, ref(database, 'users/' + uid + "/" + nameInput.value));
-      window.location = "/main.html#" + tempTime + "-" + nameInput.value;
+      window.location = "./main.html#" + tempTime + "-" + nameInput.value;
     });
   }
   let winHash = window.location.hash;
